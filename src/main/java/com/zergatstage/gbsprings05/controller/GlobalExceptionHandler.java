bf5ee202deab265fb7843a83038f52ffc81efaef
zzergAtStage <hightemplar98@iclod.com>
2024-02-13 01:35:14 +0100

    doc: added some documentation

--- src/main/java/com/zergatstage/gbsprings05/controller/GlobalExceptionHandler.java
+++ src/main/java/com/zergatstage/gbsprings05/controller/GlobalExceptionHandler.java
@@ -6,9 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Global exception handler to catch and handle exceptions thrown by controllers.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Handles NoDataFoundException and returns a ResponseEntity with a 404 status code.
+     *
+     * @param ex The NoDataFoundException to handle.
+     * @return ResponseEntity with a message and a status code.
+     */
     @ExceptionHandler(NoDataFoundException.class)
     public ResponseEntity<String> handleDataNotFoundException(NoDataFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
--- src/main/java/com/zergatstage/gbsprings05/services/UserProjectService.java
+++ src/main/java/com/zergatstage/gbsprings05/services/UserProjectService.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing user-project relationships.
+ */
 @Service
 public class UserProjectService {
     @Autowired
@@ -23,12 +26,26 @@ public class UserProjectService {
     @Autowired
     private final UsersProjectRepository usersProjectRepository;
 
+    /**
+     * Constructor for UserProjectService.
+     *
+     * @param userRepository         Repository for User entities.
+     * @param projectRepository      Repository for Project entities.
+     * @param usersProjectRepository Repository for UsersProject entities.
+     */
     public UserProjectService(UserRepository userRepository, ProjectRepository projectRepository
             , UsersProjectRepository usersProjectRepository){
         this.userRepository = userRepository;
         this.projectRepository = projectRepository;
         this.usersProjectRepository = usersProjectRepository;
     }
+
+    /**
+     * Get a list of users associated with a given project.
+     *
+     * @param projectId The ID of the project.
+     * @return A list of users associated with the project.
+     */
     public List<User> getUsersByProjectId(Long projectId){
         List<UsersProject> usersProjects = usersProjectRepository.findAllByProjectId(projectId);
         List<User> users = new ArrayList<>();
@@ -38,6 +55,13 @@ public class UserProjectService {
         }
         return users;
     }
+
+    /**
+     * Get a list of projects associated with a given user.
+     *
+     * @param userId The ID of the user.
+     * @return A list of projects associated with the user.
+     */
     public List<Project> getProjectsByUserId(Long userId) {
         List<UsersProject> usersProjects = usersProjectRepository.findAllByUserId(userId);
         System.out.println("Debug: usersProjects.size=" + usersProjects.size());
@@ -49,14 +73,28 @@ public class UserProjectService {
         System.out.println("Debug: projects.size=" + projects.size());
         return projects;
     }
+
+    /**
+     * Add a user to a project.
+     *
+     * @param userId    The ID of the user.
+     * @param projectId The ID of the project.
+     * @return The UsersProject entity representing the relationship.
+     */
     public UsersProject addUserToProject(Long userId, Long projectId) {
         UsersProject usersProject = new UsersProject();
         usersProject.setUserId(userId);
         usersProject.setProjectId(projectId);
         return usersProjectRepository.save(usersProject);
     }
+
+    /**
+     * Remove a user from a project.
+     *
+     * @param userId    The ID of the user.
+     * @param projectId The ID of the project.
+     */
     public void removeUserFromProject(Long userId, Long projectId) {
         usersProjectRepository.deleteByUserIdAndProjectId(userId, projectId);
     }
-
 }
